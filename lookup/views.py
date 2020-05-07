2c8850d824b1379d723f2ea7ff48df0d3d228c0f
sandyhpcr7 <sandeepanmohanty2011@gmail.com>
2020-05-07 20:10:58 +0530

    Third Commit

--- lookup/views.py
+++ lookup/views.py
@@ -3,7 +3,20 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-	return render(request,"home.html",{})
+	import json
+	import requests
+
+	api_rquest= requests.get("http://api.openweathermap.org/data/2.5/weather?zip={zip code},{country code}&appid={215f0416ba9e09387babe40ef5517a81}Parameters:")
+	
+	try:
+		api = json.loads(api_request.content)
+
+	except Exception as e:
+		api="Error..."
+
+
+
+	return render(request,"home.html",{'api':api})
 
 def about(request):
 	return render(request,"about.html",{})
